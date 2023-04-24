e1e8415f3c76d70f41007c723782158276352005
paolocrosetto <paolo.crosetto@gmail.com>
2023-04-24 13:38:52 +0200

    added parametric tests and simplified the code with pairwise.x.test commands

--- Scripts/demographic_tests.R
+++ Scripts/demographic_tests.R
@@ -10,94 +10,80 @@ test_demo <- df %>%
   select(ID, treatment, age, gender, final_payoff) %>%
   distinct()
 
-# gender
+#### gender ####
+# non-parametric
 kruskal.test(gender ~ treatment, data = test_demo) %>% tidy()
 
-# age
+# parametric
+chisq.test(test_demo$gender, test_demo$treatment) %>% tidy()
+
+#### age ####
+
+## non-parametric
+
 kruskal.test(age ~ treatment, data = test_demo) %>% tidy() ## significant
 
 # why is age significant?
-# click vs slider: sign
-wilcox.test(
-  test_demo$age[test_demo$treatment == "Click-and-Drag"],
-  test_demo$age[test_demo$treatment == "Slider"]
-) %>% tidy()
 
-# click vs text: NS
-wilcox.test(
-  test_demo$age[test_demo$treatment == "Click-and-Drag"],
-  test_demo$age[test_demo$treatment == "Text"]
-) %>% tidy()
+pairwise.wilcox.test(test_demo$age, test_demo$treatment, p.adjust.method = "none")
 
-# click vs distro: NS
-wilcox.test(
-  test_demo$age[test_demo$treatment == "Click-and-Drag"],
-  test_demo$age[test_demo$treatment == "Distribution"]
-) %>% tidy()
 
-# slider vs Text: sign
-wilcox.test(
-  test_demo$age[test_demo$treatment == "Slider"],
-  test_demo$age[test_demo$treatment == "Text"]
-) %>% tidy()
 
-# slider vs distribution: sign
-wilcox.test(
-  test_demo$age[test_demo$treatment == "Slider"],
-  test_demo$age[test_demo$treatment == "Distribution"]
-) %>% tidy()
+## parametric
+anova(lm(age ~ treatment, data = test_demo)) %>% tidy() ## significant
 
-# distribution vs Text: NS
-wilcox.test(
-  test_demo$age[test_demo$treatment == "Text"],
-  test_demo$age[test_demo$treatment == "Distribution"]
-) %>% tidy()
-
-# payoffs
-# age
-kruskal.test(final_payoff ~ treatment, data = test_demo) %>% tidy() ## significant
 
 # why is age significant?
 # click vs slider: sign
-wilcox.test(
-  test_demo$final_payoff[test_demo$treatment == "Click-and-Drag"],
-  test_demo$final_payoff[test_demo$treatment == "Slider"]
-) %>% tidy()
+age_test <- pairwise.t.test(test_demo$age, test_demo$treatment, p.adjust.method = "none") %>% tidy()
 
-# click vs text: NS
-wilcox.test(
-  test_demo$final_payoff[test_demo$treatment == "Click-and-Drag"],
-  test_demo$final_payoff[test_demo$treatment == "Text"]
-) %>% tidy()
-
-# click vs distro: NS
-wilcox.test(
-  test_demo$final_payoff[test_demo$treatment == "Click-and-Drag"],
-  test_demo$final_payoff[test_demo$treatment == "Distribution"]
-) %>% tidy()
-
-# slider vs Text: sign
-wilcox.test(
-  test_demo$final_payoff[test_demo$treatment == "Slider"],
-  test_demo$final_payoff[test_demo$treatment == "Text"]
-) %>% tidy()
-
-# slider vs distribution: sign
-wilcox.test(
-  test_demo$final_payoff[test_demo$treatment == "Slider"],
-  test_demo$final_payoff[test_demo$treatment == "Distribution"]
-) %>% tidy()
-
-# distribution vs Text: NS
-wilcox.test(
-  test_demo$final_payoff[test_demo$treatment == "Text"],
-  test_demo$final_payoff[test_demo$treatment == "Distribution"]
-) %>% tidy()
+age_cohen <- test_demo %>% 
+  coh_d(age~treatment)
+  
+age_test %>% 
+  left_join(age_cohen, by = c("group1" = "treatment_ref", "group2" = "treatment_foc")) %>% 
+  arrange(p.value)
 
 
-# testing control questions
+
+#### payoffs ####
+
+## non parametric
+kruskal.test(final_payoff ~ treatment, data = test_demo) %>% tidy() ## significant
+
+# why is final payoff significant?
+# click vs slider: sign
+pairwise.wilcox.test(test_demo$final_payoff, test_demo$treatment, p.adjust.method = "none") %>% tidy()
+
+## parametric
+anova(lm(final_payoff ~ treatment, data = test_demo)) %>% tidy() ## significant
+
+
+# why is payoff difference significant?
+# click vs slider: sign
+pay_test <- pairwise.t.test(test_demo$final_payoff, test_demo$treatment, p.adjust.method = "none") %>% tidy()
+
+pay_cohen <- test_demo %>% 
+  coh_d(final_payoff~treatment)
+
+pay_test %>% 
+  left_join(age_cohen, by = c("group1" = "treatment_ref", "group2" = "treatment_foc")) %>% 
+  arrange(p.value) %>% 
+  mutate(p.value = round(p.value, 3))
+
+#### control questions ####
+
+## non parametric
 df <- df %>%
   mutate(noerrors = CQerrors == 0)
 
 # not sig
 kruskal.test(noerrors ~ treatment, data = df %>% select(ID, treatment, noerrors) %>% distinct()) %>% tidy()
+
+
+## parametric
+chisq_data <- df %>% 
+  select(ID, treatment, noerrors) %>% 
+  distinct()  
+
+chisq.test(chisq_data$noerrors, chisq_data$treatment) %>% tidy()
